b3ea5aa49168559105b2dc65814cd2516ffc7f4a
Nitish-Rajendran <nitishrajendran.work@gmail.com>
2026-07-17 09:28:46 +0530

    react/session2: complete activity tasks

---
+++ react/session2/src/Avatar.tsx
@@ -0,0 +1,17 @@
+interface AvatarProps {
+  name: string
+}
+
+// This component is responsible only for displaying the user's avatar.
+
+function Avatar({ name }: AvatarProps) {
+  return (
+    <img
+      src={`https://i.pravatar.cc/50?u=${name}`}
+      alt={name}
+      style={{ borderRadius: '50%', width: 50, height: 50 }}
+    />
+  )
+}
+
+export default Avatar
